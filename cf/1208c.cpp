1f49d64207db5be117a9d82e3f3340850206ea66
HhTtLllL <1430249706@qq.com>
2019-08-27 21:42:42 +0800

    algorithm

---
+++ cf/1208c.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+int a[1010][1010];
+
+int main()
+{
+	int n;
+	cin >> n;
+	for(int i = 0;i < n;i++)
+	{
+		for(int j = 0;j < n;j++)
+		{
+			a[i][j] = (n/4 * (i/4) + j/4) * 16 + i%4*4 + j%4;
+		}
+	}
+
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			printf("%d%c", a[i][j], " \n"[j == n - 1]);
+
+	return 0;
+}
