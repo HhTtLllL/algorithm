d3c908e5d1979ee51bef7a6c699b798319dec4ca
HhTtLllL <1430249706@qq.com>
2019-09-02 17:59:21 +0800

    概率dp

--- poj/2096.cpp
+++ poj/2096.cpp
@@ -20,16 +20,18 @@ int main()
 			for(int j = s;j >= 0;j--)
 			{
 				if(i == n && j == s) continue;
-				int p1 = (n - i) * (s - j)*1.0;
-				int p2 = (n - i) * j * 1.0;
-				int p3 = i * (s - j) * 1.0;
-				int p4 = i * j * 1.0;
-				dp[i][j] = (p1*dp[i + 1][j + 1] + p2*dp[i+1][j] + p3*dp[i][j+1] + n *s);
+				double temp = n*s - i*j;
+				double p1 = n*s;
+				double p2 = i * (s - j);
+				double p3 = (n - i) * j;
+				double p4 = (n - i) * (s - j);
+
+				dp[i][j] = (p1/temp + p2/temp*dp[i][j + 1] + p3/temp*dp[i + 1][j] + p4/temp*dp[i + 1][j+1]);
 
 			}
 		}
 
-		printf("%.4lf\n",dp[0][0]);
+		printf("%.4f\n",dp[0][0]);
 //		printf("1111\n");
 	}
 
