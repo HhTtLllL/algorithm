d2b6590b7c6a732fe4ccbb85f33ea04fa6e03729
HhTtLllL <1430249706@qq.com>
2020-04-28 20:32:00 +0800

    update

---
+++ leetcode/225.cpp
@@ -0,0 +1,45 @@
+//##################################################################
+// File Name: 225.cpp
+// File Effect: 
+// Author: tttt
+// mail: tttt@xiyoulinux.org
+// Created Time: 2020年04月27日 星期一 21时07分46秒
+//=============================================================
+//!/usr/bin/python
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    MyStack() {
+	    queue<int> q;
+
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+
+    }
+    
+    /** Get the top element. */
+    int top() {
+
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
