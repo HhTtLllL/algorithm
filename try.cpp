74fdd87e0ae2424878cba8e547d94ca72b5029d0
HhTtLllL <1430249706@qq.com>
2020-11-28 23:12:38 +0800

    update

---
+++ try.cpp
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////
+// File Name: try.cpp
+// Author: tt
+// Email: tttt@xiyoulinux.org
+// Created Time : 2020-11-07 22:54:24
+// Description:
+///////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+void test()
+try {
+
+
+}
+catch (const exception& e){
+    
+
+}
+
+
+int main() {
+
+
+    return 0;
+}
